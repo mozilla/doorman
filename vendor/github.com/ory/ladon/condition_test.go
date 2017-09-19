c3d8af8b87244319e64b77fe3558e285bbec1e31
Mathieu Leplatre <mathieu@mozilla.com>
2017-09-19 18:29:13 +0200

    Vendor update

---
+++ vendor/github.com/ory/ladon/condition_test.go
@@ -0,0 +1,73 @@
+package ladon
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConditionsAppend(t *testing.T) {
+	cs := Conditions{}
+	c := &CIDRCondition{}
+	cs.AddCondition("clientIP", c)
+	assert.Equal(t, c, cs["clientIP"])
+}
+
+func TestMarshalUnmarshalNative(t *testing.T) {
+	css := &Conditions{
+		"clientIP": &CIDRCondition{CIDR: "127.0.0.1/0"},
+		"owner":    &EqualsSubjectCondition{},
+	}
+	out, err := json.Marshal(css)
+	require.Nil(t, err)
+	t.Logf("%s", out)
+
+	cs := Conditions{}
+	require.Nil(t, cs.UnmarshalJSON(out))
+}
+
+func TestMarshalUnmarshal(t *testing.T) {
+	css := &Conditions{
+		"clientIP": &CIDRCondition{CIDR: "127.0.0.1/0"},
+		"owner":    &EqualsSubjectCondition{},
+		"role":     &StringMatchCondition{Matches: ".*"},
+	}
+	out, err := json.Marshal(css)
+	require.Nil(t, err)
+	t.Logf("%s", out)
+
+	cs := Conditions{}
+	require.Nil(t, json.Unmarshal([]byte(`{
+	"owner": {
+		"type": "EqualsSubjectCondition"
+	},
+	"clientIP": {
+		"type": "CIDRCondition",
+		"options": {
+			"cidr": "127.0.0.1/0"
+		}
+	},
+	"role": {
+		"type": "StringMatchCondition",
+		"options": {
+			"matches": ".*"
+		}
+	}
+}`), &cs))
+
+	require.Len(t, cs, 3)
+	assert.IsType(t, &EqualsSubjectCondition{}, cs["owner"])
+	assert.IsType(t, &CIDRCondition{}, cs["clientIP"])
+	assert.IsType(t, &StringMatchCondition{}, cs["role"])
+}
+
+func TestUnmarshalFails(t *testing.T) {
+	cs := Conditions{}
+	require.NotNil(t, json.Unmarshal([]byte(`{
+	"somefield": {
+		"type": "DoesntExist"
+	}
+}`), &cs))
+}
