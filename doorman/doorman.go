6597af217f5db40507e64482735051ca68b34b34
Mathieu Leplatre <mathieu@mozilla.com>
2017-11-01 11:20:51 +0100

    Split doorman and handler

--- doorman/doorman.go
+++ doorman/doorman.go
@@ -4,15 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 
-	"github.com/gin-gonic/gin"
 	"github.com/ory/ladon"
 	manager "github.com/ory/ladon/manager/memory"
 	log "github.com/sirupsen/logrus"
-	jwt "gopkg.in/square/go-jose.v2/jwt"
 	"gopkg.in/yaml.v2"
 
 	"github.com/leplatrem/iam/utilities"
@@ -20,12 +17,6 @@ import (
 
 // DefaultPoliciesFilename is the default policies filename.
 const DefaultPoliciesFilename string = "policies.yaml"
-
-// DoormanContextKey is the Gin context key to obtain the *Doorman instance.
-const DoormanContextKey string = "doorman"
-
-// JWTContextKey is the Gin context key to obtain the *jwt.Claims instance.
-const JWTContextKey string = "JWT"
 
 const maxInt int64 = 1<<63 - 1
 
@@ -134,75 +125,4 @@ func loadConfiguration(filename string) (*Configuration, error) {
 	}
 
 	return &config, nil
-}
-
-// ContextMiddleware adds the Doorman instance to the Gin context.
-func ContextMiddleware(doorman *Doorman) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Set(DoormanContextKey, doorman)
-		c.Next()
-	}
-}
-
-// SetupRoutes adds doorman views to query the policies.
-func SetupRoutes(r *gin.Engine, doorman *Doorman) {
-	r.Use(ContextMiddleware(doorman))
-	if doorman.JWTIssuer != "" {
-		validator := &Auth0Validator{
-			Issuer: doorman.JWTIssuer,
-		}
-		r.Use(VerifyJWTMiddleware(validator))
-	} else {
-		log.Warning("No JWT issuer configured. No authentication will be required.")
-	}
-	r.POST("/allowed", allowedHandler)
-}
-
-func allowedHandler(c *gin.Context) {
-	if c.Request.ContentLength == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Missing body",
-		})
-		return
-	}
-
-	var accessRequest ladon.Request
-	if err := c.BindJSON(&accessRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	payloadJWT, ok := c.Get(JWTContextKey)
-	// Is VerifyJWTMiddleware enabled? (disabled in tests)
-	if ok {
-		claims := payloadJWT.(*jwt.Claims)
-		// Subject is taken from JWT.
-		accessRequest.Subject = claims.Subject
-	}
-
-	doorman := c.MustGet(DoormanContextKey).(*Doorman)
-
-	origin := c.Request.Header.Get("Origin")
-
-	// Will fail if origin is unknown.
-	err := doorman.IsAllowed(origin, &accessRequest)
-	allowed := (err == nil)
-
-	log.WithFields(
-		log.Fields{
-			"allowed":  allowed,
-			"subject":  accessRequest.Subject,
-			"action":   accessRequest.Action,
-			"resource": accessRequest.Resource,
-		},
-	).Info("request.authorization")
-
-	c.JSON(http.StatusOK, gin.H{
-		"allowed": allowed,
-		"user": gin.H{
-			"id": accessRequest.Subject,
-		},
-	})
 }
