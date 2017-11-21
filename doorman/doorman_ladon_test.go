a49e0fb8208dbef2d4de503faba9a2fa73e23b21
Mathieu Leplatre <mathieu@mozilla.com>
2017-11-21 11:16:44 +0100

    Update tests with latest sample

--- doorman/doorman_ladon_test.go
+++ doorman/doorman_ladon_test.go
@@ -184,7 +184,7 @@ func TestLoadGithub(t *testing.T) {
 	assert.NotNil(t, err)
 
 	// Good URL
-	w = New([]string{"https://github.com/leplatrem/iam/raw/4704cc9/sample.yaml"})
+	w = New([]string{"https://github.com/leplatrem/iam/raw/452ef7a/sample.yaml"})
 	err = w.LoadPolicies()
 	assert.Nil(t, err)
 	assert.Equal(t, len(w.configs["https://sample.yaml"].Tags), 1)
